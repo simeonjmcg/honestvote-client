353e05b92e993713efef9b816b7f83242145d7cb
Simeon McGraw <simeonjmcgraw@gmail.com>
2020-02-08 10:55:13 -0500

    Added slugify

--- app/utils.ts
+++ app/utils.ts
@@ -55,7 +55,61 @@ export function sumTicketsVotes(tickets: Ticket[]) {
                   .reduce((a, v) => a + v, 0); // sum
 }
 
+<<<<<<< Updated upstream
 /** Sort tickets according to votes */
 export function sortTickets(tickets: Ticket[]) {
     return tickets.sort((t1, t2) => countVotes(t2.votes) - countVotes(t1.votes));
+=======
+/** takes array and transformation function and returns map determined by function */
+export function mapKey<T1, T2>(array: T1[], fn: (value: T1, index: number) => { value: T2, key: string }) {
+    return array.reduce<{[key: string]: T2}>((map, value, index) => {
+        const r = fn(value, index);
+        map[r.key] = r.value;
+        return map;
+    }, {});
+}
+
+/** takes map and transformation function and returns new map, with both key and value remapped */
+export function mapKeyValueMap<T1, T2>(map: {[key: string]: T1}, fn: (value: T1, key: string) => {key: string, value: T2}) {
+    return Object.keys(map).reduce<{[key: string]: T2}>((newmap, key) => {
+        const r = fn(map[key], key);
+        newmap[r.key] = r.value;
+        return newmap;
+    }, {});
+}
+
+/** takes map and transformation function and returns new map, with a given value remapped to given key */
+export function mapValueMap<T1, T2>(map: {[key: string]: T1}, fn: (item: T1, key: string) => T2) {
+    return mapKeyValueMap(map, (value, key) => ({ value: fn(value, key), key }));
+}
+
+/** Accessor function for id */
+export function getId<T extends IdObject>(obj: T) { return obj.id; }
+
+/** Maps list to map by id */
+export function groupById<T>(list: T[], accessor: (object: T) => AppId) {
+    const byId: {[key: string]: T[]} = {};
+    list.forEach(item => {
+        const id = accessor(item);
+        if (!(id in byId)) byId[id] = [];
+        byId[id].push(item);
+    });
+    return byId;
+} 
+
+/** takes a string and returns a slugified version */
+export function slugify(input: string) {
+    const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;';
+    const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------';
+    const p = new RegExp(a.split('').join('|'), 'g');
+
+    return input.toLowerCase()
+        .replace(/\s+/g, '-') // Replace spaces with -
+        .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+        .replace(/&/g, '-and-') // Replace & with 'and'
+        .replace(/[^\w\-]+/g, '') // Remove all non-word characters
+        .replace(/\-\-+/g, '-') // Replace multiple - with single -
+        .replace(/^-+/, '') // Trim - from start of text
+        .replace(/-+$/, ''); // Trim - from end of text
+>>>>>>> Stashed changes
 }
